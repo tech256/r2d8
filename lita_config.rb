d137698d76f2d60f9a7c585abdbb31a6f8d06c8e
Jay Hayes <ur@iamvery.com>
2015-04-02 10:07:37 -0500

    Install and configure tinysong

--- lita_config.rb
+++ lita_config.rb
@@ -30,6 +30,7 @@ Lita.configure do |config|
   config.handlers.memegen.command_only = false
   config.handlers.memegen.username = ENV["MEMEGEN_USER"]
   config.handlers.memegen.password = ENV["MEMEGEN_PASS"]
+  config.handlers.tinysong.api_key = ENV["TINYSONG_KEY"]
   config.handlers.static_meme.mapping = {
     /^where('| i)s #{config.robot.name}[?]?$/i => "There is no #{config.robot.name}. There is only Zuul.",
     /^thank(s| ?you)\s#{config.robot.name}/i => "At your service.",
