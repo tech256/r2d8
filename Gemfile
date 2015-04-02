cccbbe48537f60b33bb75680aa6eac78cfd43080
Jay Hayes <ur@iamvery.com>
2015-04-01 19:52:49 -0500

    Install slack adapter

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "lita"
+gem "lita-slack"
--- lita_config.rb
+++ lita_config.rb
@@ -17,7 +17,8 @@ Lita.configure do |config|
 
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
-  config.robot.adapter = :shell
+  config.robot.adapter = :slack
+  config.adapters.slack.token = ENV.fetch("SLACK_TOKEN")
 
   ## Example: Set options for the chosen adapter.
   # config.adapter.username = "myname"
