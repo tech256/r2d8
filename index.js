411a5e16c233e9a5e4a37cb152d39a177ae40905
Tyler Hughes <tyler.hughes@hexagon.com>
2020-05-12 10:34:25 -0500

    Cleaned up whitespace

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = require( './src/server' );
 // Start the bot
 bot.startBot();
 
-// Start the server so we have a page we can load 
+// Start the server so we have a page we can load
 // Without this the app will crash because Heroku would have nothing to load
 server.listen( process.env.PORT, function() {
     logger.log( 'debug', `Server started running on port ${process.env.PORT}.` );
