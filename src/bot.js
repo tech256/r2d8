9e01f8c4f99747c9a80615273d9b345bf9737775
Tyler <2483249+PallasStreams@users.noreply.github.com>
2020-06-04 09:50:14 -0500

    Fixed where I used the wrong function for the logger

--- src/bot.js
+++ src/bot.js
@@ -63,7 +63,7 @@ const startBot = () => {
         // Only log when the bot actually has something to say
         if ( response !== '' ) {
             logger.log( 'debug', `${process.env.ROBOT_NAME} says: ${response}` );
-            logger.debug( 'debug', JSON.stringify(bot) );
+            logger.log( 'debug', JSON.stringify(bot) );
             
             // let the bot speak man!
             bot.postMessage( event.channel, response, {
