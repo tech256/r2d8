398b9a035d4f8264f01e3efaeeac2c8602d04a5c
Tomas Gallucci <professortom@gmail.com>
2020-11-29 13:20:07 -0600

    log setupDB errors via Winston

--- src/helpers/databaseHelpers.js
+++ src/helpers/databaseHelpers.js
@@ -1,19 +1,17 @@
 const db = require( '../../config/database' );
+const logger = require( '../logger' );
 
 const setupDB = async() => {
     try {
-        // console.log( 'in SetupDB' );
         await db.authenticate();
+        
+        try {
+            await db.sync();
+        } catch ( err ) {
+            logger.log( err );
+        }
     } catch ( err ) {
-        //TODO hook up to Winston
-        console.log( 'Error ' + err );
-    }
-    try {
-        // console.log( 'in dbSync' );
-        await db.sync();
-    } catch ( err ) {
-        //TODO hook up to Winston
-        console.log( 'Error ' + err );
+        logger.log( err );
     }
 };
 
