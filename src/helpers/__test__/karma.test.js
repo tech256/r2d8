0d1dba24c12a030999a10ffd25e3b617331f9c38
Tomas Gallucci <professortom@gmail.com>
2020-11-29 00:27:29 -0600

    use it instead of test

--- src/helpers/__test__/karma.test.js
+++ src/helpers/__test__/karma.test.js
@@ -10,20 +10,19 @@ describe( 'Karma', () => {
             databaseHelpers.setupDB = jest.fn();
         } );
         describe( 'phrase', () => {
-            it( 'exists', async() => {
+            test( 'exists', async() => {
                 Phrase.findAll = jest.fn().mockResolvedValue( [{}] );
                 const result = await karma.getPhraseFromDatabase( 'this is a test' );
                 expect( result ).toEqual( [{}] );
             } );
-            it( 'does not exist', async() => {
+            test( 'does not exist', async() => {
                 Phrase.findAll = jest.fn().mockResolvedValue( [] );
                 const result = await karma.getPhraseFromDatabase( 'hello' );
                 expect( result ).toEqual( [] );
             } );
         } );
-        it( 'calls setupDB only once', async() => {
+        test( 'calls setupDB only once', async() => {
             Phrase.findAll = jest.fn();
-
             await karma.getPhraseFromDatabase( 'foo bar baz' );
             expect( databaseHelpers.setupDB ).toHaveBeenCalledTimes( 1 );
         } );
