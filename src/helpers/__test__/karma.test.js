53462e74d98904e3f88751c32c32363baf6efe0c
Tomas Gallucci <professortom@gmail.com>
2020-11-29 20:32:54 -0600

    add karma.decrement()

--- src/helpers/__test__/karma.test.js
+++ src/helpers/__test__/karma.test.js
@@ -5,10 +5,10 @@ const databaseHelpers = require( '../databaseHelpers' );
 const logger = require( '../../logger' );
 
 describe( 'Karma', () => {
+    beforeEach( async() => {
+        databaseHelpers.setupDB = jest.fn();
+    } );
     describe( 'increment', () => {
-        beforeEach( async() => {
-            databaseHelpers.setupDB = jest.fn();
-        } );
         test( 'calls getPhraseFromDatabase once', async() => {
             karmaHelpers.getPhraseFromDatabase = jest.fn().mockResolvedValue( ['45 Savage'] );
             Phrase.increment = jest.fn().mockResolvedValue( 'a' );
@@ -94,6 +94,99 @@ describe( 'Karma', () => {
                     logger.log = jest.fn();
 
                     await karma.increment( 'any message' );
+                    expect( logger.log ).toHaveBeenCalledTimes( 1 );
+                    expect( logger.log ).toHaveBeenCalledWith( addError );
+                } );
+            } );
+        } );
+    } );
+    describe( 'decrement', () => {
+        test( 'calls getPhraseFromDatabase once', async() => {
+            karmaHelpers.getPhraseFromDatabase = jest.fn().mockResolvedValue( ['45 Savage'] );
+            Phrase.increment = jest.fn().mockResolvedValue( 'a' );
+            const phrase = 'foobarbaz';
+            await karma.decrement( phrase );
+            
+            await expect( karmaHelpers.getPhraseFromDatabase ).toHaveBeenCalledTimes( 1 );
+            await expect( karmaHelpers.getPhraseFromDatabase ).toHaveBeenCalledWith( phrase );
+        } );
+
+        describe( 'message exists in database', () => {
+            describe( 'Phrase.decrement', () => {
+                beforeEach( async() => {
+                    karmaHelpers.getPhraseFromDatabase = jest.fn().mockResolvedValue( ['not a real return value'] );
+                } );
+                test( 'is called once', async() => {
+                    Phrase.decrement = jest.fn().mockResolvedValue( 'a' ).mockName( 'increment mock' );
+    
+                    await karma.decrement( 'foo' );
+                    expect( Phrase.decrement ).toHaveBeenCalledTimes( 1 );
+    
+                    const foo = {
+                        plain: true,
+                        returning: true,
+                        where: {
+                            message: 'foo'
+                        }
+                    };
+                    expect( Phrase.decrement ).toHaveBeenCalledWith( 'points', foo );
+                } );
+                test( 'returns "phrase: pointValue"', async() => {
+                    const annoyingReturnObject = [
+                        [{
+                            message: 'bye bye love',
+                            points: '-5'
+                        }]
+                    ];
+                    Phrase.decrement = jest.fn().mockResolvedValue( annoyingReturnObject ).mockName( 'increment mock' );
+    
+                    const returnedString = await karma.decrement( 'foo' );
+                    expect( returnedString ).toEqual( 'bye bye love: -5' );
+                } );
+
+                test( 'logs error on throw', async() => {
+                    const incrementError = new Error( 'increment' );
+                    Phrase.decrement = jest.fn().mockImplementation( () => {
+                        throw incrementError;
+                    } );
+                    
+                    logger.log = jest.fn();
+
+                    await karma.decrement( 'foobarbaz' );
+                    expect( logger.log ).toHaveBeenCalledTimes( 1 );
+                    expect( logger.log ).toHaveBeenCalledWith( incrementError );
+                } );
+            } );
+        } );
+        describe( 'message does not exist in database', () => {
+            test( 'returns "message: points"', async() => {
+                karmaHelpers.getPhraseFromDatabase = jest.fn().mockResolvedValue( [] );
+                karmaHelpers.addPhrase = jest.fn().mockResolvedValue( {message: 'a message', points: 33} );
+
+                const returnValue = await karma.decrement( 'some message' );
+                expect( returnValue ).toEqual( 'a message: 33' );
+            } );
+            describe( 'karmaHelpers.addPhrase', () => {
+                beforeEach( async() => {
+                    karmaHelpers.getPhraseFromDatabase = jest.fn().mockResolvedValue( [] );
+                } );
+                test( 'is called once', async() => {
+                    karmaHelpers.addPhrase = jest.fn();
+                    const message = 'test message';
+                    await karma.decrement( message );
+                    
+                    expect( karmaHelpers.addPhrase ).toHaveBeenCalledTimes( 1 );
+                    expect( karmaHelpers.addPhrase ).toHaveBeenCalledWith( message, -1 );
+                } );
+                test( 'throws error', async() => {
+                    const addError = new Error( 'add error' );
+                    karmaHelpers.addPhrase = jest.fn().mockImplementation( () => {
+                        throw addError;
+                    } );
+
+                    logger.log = jest.fn();
+
+                    await karma.decrement( 'any message' );
                     expect( logger.log ).toHaveBeenCalledTimes( 1 );
                     expect( logger.log ).toHaveBeenCalledWith( addError );
                 } );
--- src/helpers/karma.js
+++ src/helpers/karma.js
@@ -17,15 +17,11 @@ const increment = async( message ) => {
                 returning: true,
                 plain: true
             } );
-            // console.log( 'back from inc: ' + JSON.stringify( returnedObject, null, 2 ) );
-            // console.log( 'keys: ' + Object.keys( returnedObject ) );
             const dataValues = returnedObject[0][0];
             return dataValues.message + ': ' + dataValues.points;
         } catch ( err ) {
             logger.log( err );
         }
-
-        // add record
     } else {
         try {
             const newPhrase = await karmaHelpers.addPhrase( message, 1 );
@@ -36,4 +32,32 @@ const increment = async( message ) => {
     }
 };
 
-module.exports = {increment};
+const decrement = async( message ) => {
+    const phrase = await karmaHelpers.getPhraseFromDatabase( message );
+
+    if ( phrase.length > 0 ) {
+        try {
+            const returnedObject = await Phrase.decrement( 'points', {
+                where: {
+                    message: message
+                },
+                returning: true,
+                plain: true
+            } );
+
+            const dataValues = returnedObject[0][0];
+            return dataValues.message + ': ' + dataValues.points;
+        } catch ( err ) {
+            logger.log( err );
+        }
+    } else {
+        try {
+            const newPhrase = await karmaHelpers.addPhrase( message, -1 );
+            return newPhrase.message + ': ' + newPhrase.points;
+        } catch ( err ) {
+            logger.log( err );
+        }
+    }
+};
+
+module.exports = {increment, decrement};
