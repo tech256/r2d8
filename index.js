bb60f7e4841e7579b9282db5767e93f14d680021
Tomas Gallucci <professortom@gmail.com>
2020-11-30 23:10:31 -0600

    restore index.js to its former glory

--- index.js
+++ index.js
@@ -2,32 +2,12 @@ require( 'dotenv' ).config();
 const logger = require( './src/logger' );
 const bot = require( './src/bot' );
 const server = require( './src/server' );
-const karma = require( './src/helpers/karma' );
-//require database
-const db = require( './config/database' );
-const sequelize = require( 'sequelize' );
-
-//testDB
-db.authenticate()
-    .then( () => console.log( 'database connected...' ) )
-    .catch( ( err ) => console.log( 'Error ' + err ) );
-
-const data = {
-    message: 'this is a test',
-    points: 32,
-};
-
-db.sync().then( () => {
-    // karma.addPhrase();
-    const result = karma.incrementPoint( data.message, data.points );
-    console.log( result );
-} );
 
 // Start the bot
-// bot.startBot();
+bot.startBot();
 
 // Start the server so we have a page we can load
 // Without this the app will crash because Heroku would have nothing to load
-// server.listen( process.env.PORT, function() {
-//     logger.log( 'debug', `Server started running on port ${process.env.PORT}.` );
-// } );
+server.listen( process.env.PORT, function() {
+    logger.log( 'debug', `Server started running on port ${process.env.PORT}.` );
+} );
