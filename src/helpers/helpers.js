ad5b6ea5c0e0aa1526ecae7ed2967efa6810407f
Tomas Gallucci <professortom@gmail.com>
2020-12-03 23:21:10 -0600

    add extraction functions

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -10,6 +10,45 @@ const isEmpty = function( obj ) {
     return ( obj == null ) || ( obj === '' ) || ( Array.isArray( obj ) && obj.length === 0 );
 };
 
+const extractFromSingleQuotes = ( phrase ) => {
+    const regEx = new RegExp( '^[\'].*[\']$' );
+    let stringToReturn = '';
+
+    if( phrase.match( regEx ) ) {
+        stringToReturn = phrase.substr( 1, phrase.length );
+        stringToReturn = stringToReturn.substr( 0, phrase.length - 2 );
+    }
+
+    return stringToReturn;
+};
+
+const extractFromDoubleQuotes = ( phrase ) => {
+    const regEx = new RegExp( '^["].*["]$' );
+    let stringToReturn = '';
+
+    if( phrase.match( regEx ) ) {
+        stringToReturn = phrase.substr( 1, phrase.length );
+        stringToReturn = stringToReturn.substr( 0, phrase.length - 2 );
+    }
+
+    return stringToReturn;
+};
+
+const extractFromParentheses = ( phrase ) => {
+    const regEx = new RegExp( '^[(].*[)]$' );
+    let stringToReturn = '';
+
+    if( phrase.match( regEx ) ) {
+        stringToReturn = phrase.substr( 1, phrase.length );
+        stringToReturn = stringToReturn.substr( 0, phrase.length - 2 );
+    }
+
+    return stringToReturn;
+};
+
 module.exports = {
-    isEmpty
-}
+    isEmpty,
+    extractFromSingleQuotes,
+    extractFromDoubleQuotes,
+    extractFromParentheses
+};
