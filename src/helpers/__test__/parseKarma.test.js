cd8a074295f59f5bfab90792bbdea005a314dd16
Tomas Gallucci <professortom@gmail.com>
2020-12-27 10:21:52 -0600

    refactors

--- src/helpers/__test__/parseKarma.test.js
+++ src/helpers/__test__/parseKarma.test.js
@@ -8,44 +8,121 @@ describe( 'parseKarma', () => {
         event.text = '';
     } );
     describe( 'increment', () => {
-        beforeEach( async() => {
-            karma.increment = jest.fn().mockResolvedValue( expectedString );
-        } );
-        const expectedString = 'foo: 1';
-        test( 'calls karma.increment and returns its result ', async() => {
-            event.text = 'foo++';
+        describe( 'single word', () => {
+            test( 'simple case', async() => {
+                event.text = 'racecar++';
+
+                karma.increment = jest.fn().mockResolvedValue( 'inconsequental to this test' );
             
-            const result = await parseKarma.handleKarma( event );
-            expect( karma.increment ).toHaveBeenCalledTimes( 1 );
-            expect( result ).toEqual( expectedString );
+                await parseKarma.handleKarma( event );
+                expect( karma.increment ).toHaveBeenCalledTimes( 1 );
+                expect( karma.increment ).toHaveBeenCalledWith( 'racecar' );
+            } );
+            test( 'in middle of string ', async() => {
+                event.text = 'this is the foo++ song that never ends';
+                
+                karma.increment = jest.fn().mockResolvedValue( 'scooby-dooby-do' );
+                
+                await parseKarma.handleKarma( event );
+                expect( karma.increment ).toHaveBeenCalledTimes( 1 );
+                expect( karma.increment ).toHaveBeenCalledWith( 'foo' );
+            } );
         } );
-        test( 'in middle of string ', async() => {
-            event.text = 'this is the foo++ song that never ends';
           
-            const result = await parseKarma.handleKarma( event );
-            expect( karma.increment ).toHaveBeenCalledTimes( 1 );
-            expect( result ).toEqual( expectedString );
+        describe( 'phrase', () => {
+            test( 'surrounded by single quotes', async()=> {
+                event.text = '\'round the rugged rock the ragged rascal ran\'++';
+              
+                const expectedString = 'round the rugged rock the ragged rascal ran';
+                karma.increment = jest.fn()
+                    .mockResolvedValue( 'in the middle of the night' )
+                    .mockName( 'ki' );
+            
+                await parseKarma.handleKarma( event );
+                expect( karma.increment ).toHaveBeenCalledTimes( 1 );
+                expect( karma.increment ).toHaveBeenCalledWith( expectedString );
+            } );
+
+            test( 'surrounded by double quotes', async()=> {
+                event.text = '"double bubble double trouble"++';
+              
+                const expectedString = 'double bubble double trouble';
+                karma.increment = jest.fn().mockResolvedValue( 'everybody plays the fool' );
+            
+                await parseKarma.handleKarma( event );
+                expect( karma.increment ).toHaveBeenCalledTimes( 1 );
+                expect( karma.increment ).toHaveBeenCalledWith( expectedString );
+            } );
+
+            test( 'surrounded by parentheses', async()=> {
+                event.text = '(your mamas on crack rock)++';
+              
+                const expectedString = 'your mamas on crack rock';
+                karma.increment = jest.fn().mockResolvedValue( 'I bet you weren\'t expecting this' );
+            
+                await parseKarma.handleKarma( event );
+                expect( karma.increment ).toHaveBeenCalledTimes( 1 );
+                expect( karma.increment ).toHaveBeenCalledWith( expectedString );
+
+            } );
         } );
     } );
     describe( 'decrement', () => {
-        const expectedString = 'foo: -1';
+        describe( 'single word', () => {
+            test( 'simple case ', async() => {
+                event.text = 'foo--';
+        
+                karma.decrement = jest.fn().mockResolvedValue( 'don\'t care' );
+            
+                await parseKarma.handleKarma( event );
+                expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
+                expect( karma.decrement ).toHaveBeenCalledWith( 'foo' );
+            } );
+            test( 'in middle of string ', async() => {
+                event.text = 'this is the oof-- that never ends';
+            
+                const expectedString = 'oof';
+                karma.decrement = jest.fn().mockResolvedValue( 'I\'ll have a blue Christmas' );
+            
+                await parseKarma.handleKarma( event );
+                expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
+                expect( karma.decrement ).toHaveBeenCalledWith( expectedString );
+            } );
+        } );
 
-        beforeEach( () => {
-            karma.decrement = jest.fn().mockResolvedValue( expectedString );
-        } );
-        test( 'calls karma.decrement and returns its result ', async() => {
-            event.text = 'foo--';
+        describe( 'phrase', () => {
+            test( 'surrounded by single quotes', async()=> {
+                event.text = '\'I am the monarch of the sea\'--';
             
-            const result = await parseKarma.handleKarma( event );
-            expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
-            expect( result ).toEqual( expectedString );
-        } );
-        test( 'in middle of string ', async() => {
-            event.text = 'this is the foo-- that never ends';
+                const expectedString = 'I am the monarch of the sea';
+                karma.decrement = jest.fn().mockResolvedValue( 'fa la la la la' );
+          
+                await parseKarma.handleKarma( event );
+                expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
+                expect( karma.decrement ).toHaveBeenCalledWith( expectedString );
+            } );
+
+            test( 'surrounded by double quotes', async()=> {
+                event.text = '"Good King Wenceslas looked out On the Feast of Stephen"--';
             
-            const result = await parseKarma.handleKarma( event );
-            expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
-            expect( result ).toEqual( expectedString );
+                const expectedString = 'Good King Wenceslas looked out On the Feast of Stephen';
+                karma.decrement = jest.fn().mockResolvedValue( 'foo better best' );
+          
+                await parseKarma.handleKarma( event );
+                expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
+                expect( karma.decrement ).toHaveBeenCalledWith( expectedString );
+            } );
+
+            test( 'surrounded by parentheses', async()=> {
+                event.text = '(that dont impress me much)--';
+            
+                const expectedString = 'that dont impress me much';
+                karma.decrement = jest.fn().mockResolvedValue( expectedString );
+          
+                await parseKarma.handleKarma( event );
+                expect( karma.decrement ).toHaveBeenCalledTimes( 1 );
+                expect( karma.decrement ).toHaveBeenCalledWith( expectedString );
+            } );
         } );
     } );
 
--- src/helpers/parseKarma.js
+++ src/helpers/parseKarma.js
@@ -5,8 +5,6 @@ const helpers = require( './helpers' );
 const handleKarma = async( event ) => {
     const message = event.text;
     
-    const addKarma = new RegExp( '\\w*[+][+]' );
-    const subtractKarma = new RegExp( '\\w*[-][-]' );
     const topKarma = new RegExp( '^!karma --top$|^!karma -t$' );
     const bottomKarma = new RegExp( '^!karma --bottom$|^!karma -b$' );
     const points = new RegExp( '^!karma .+$' );
@@ -18,24 +16,71 @@ const handleKarma = async( event ) => {
   
     let response = '';
   
-    if ( message.match( addKarma ) ) {
-        // pull ++ off end of string
-        let noIncrementString = message.substring( 0, message.length - 2 );
-        // TODO: remove '', "", or ()
-        response = await karma.increment( noIncrementString );
+    response = await handleIncrement( message );
+    if ( !( isEmpty( response ) ) ) {
+        return response;
     }
-      
-    else if( message.match( subtractKarma ) ) {
-        // pull -- off end of string
-        let noDecrementString = message.substring( 0, message.length - 2 );
-        // TODO: remove '', "", or ()
-          
-        response = await karma.decrement( noDecrementString );
+    
+    response = await handleDecrement( message );
+    if ( !( isEmpty( response ) ) ) {
+        return response;
     }
-
-    return response;
+    
 };
 
+const handleIncrement = async( message ) => {
+    let response = '';
+  
+    const addKarma = new RegExp( '(\\w*[+][+]|^[\'].+[\'][+][+]|^["].+["][+][+]|^[(].+[)][+][+])' );
+    const matched = message.match ( addKarma );
+
+    if ( !( isEmpty( matched ) ) ) {
+        // if we match, then we get back an array of matches. we only care about the first match
+        const phrase = matched[0];
+
+        // pull ++ off end of string
+        const noIncrementString = phrase.substring( 0, phrase.length - 2 );
+        
+        // remove surrounding single or double quotes OR parentheses
+        const extracted = extractAsNeeded( noIncrementString );
+
+        // we're empty only if none of the above
+        // enclosing punctuations were stripped off
+        if( !( isEmpty( extracted ) ) ) {
+            response = await karma.increment( extracted );
+        } else {
+            response = await karma.increment( noIncrementString );
+        }
+        return response;
+    }
+};
+
+const handleDecrement = async( message ) => {
+    let response = '';
+  
+    const subtractKarma = RegExp( '(\\w*[-][-]|^[\'].+[\'][-][-]|^["].+["][-][-]|^[(].+[)][-][-])' );
+    const matched = message.match( subtractKarma );
+    
+    if ( !( isEmpty( matched ) ) ) {
+        // if we match, then we get back an array of matches. we only care about the first match
+        const phrase = matched[0];
+
+        // pull -- off end of string
+        const noDecrementString = phrase.substring( 0, phrase.length - 2 );
+      
+        // remove surrounding single or double quotes OR parentheses
+        const extracted = extractAsNeeded( noDecrementString );
+
+        // we're empty only if none of the above
+        // enclosing punctuations were stripped off
+        if( !( isEmpty( extracted ) ) ) {
+            response = await karma.decrement( extracted );
+        } else {
+            response = await karma.decrement( noDecrementString );
+        }
+        return response;
+    }
+};
 const extractAsNeeded = ( phrase ) => {
     const singleQuoteExtractionResult = helpers.extractFromSingleQuotes( phrase );
     if ( !( isEmpty( singleQuoteExtractionResult ) ) ) {
@@ -51,6 +96,8 @@ const extractAsNeeded = ( phrase ) => {
     if( !( isEmpty( parenthesesExtractionResult ) ) ) {
         return parenthesesExtractionResult;
     }
+
+    return phrase;
 };
 
 module.exports = {handleKarma, extractAsNeeded};
