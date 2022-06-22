08d3edb9f2fed77139c09c3149b9c5a676d9bbab
Tyler Hughes <tyler.hughes@pfasolutions.com>
2022-06-22 09:32:59 -0500

    Removed unused commented code

--- config/database.js
+++ config/database.js
@@ -11,26 +11,3 @@ db.pool = {
 };
 
 module.exports = db;
-/*
-module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        define: {
-            createdAt: 'createdat',
-            updatedAt: 'updatedat',
-        },
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        native: true,
-
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
-);
-*/
