485617d6182030efcc1b03f84c892506639f2b55
Tyler Hughes <tyler.hughes@pfasolutions.com>
2022-06-22 09:24:20 -0500

    Fixed typo

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require( 'sequelize' );
 
-const db = new Sequelize( env.DATABASE_URL );
+const db = new Sequelize( process.env.DATABASE_URL );
 db.dialect = 'postgres';
 db.native = true;
 db.pool = {
